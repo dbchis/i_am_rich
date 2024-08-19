69dc60e8182924fa0b2afda4fed7f0202bca805d
chidb2k3 <dangbachi345@gmail.com>
2024-08-19 22:36:49 +0700

    ht

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("I am rich"),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text(
+            "I Am Rich",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/kc.png'),
+            // NetworkImage(
+            //     'https://kitadiamonds.com.vn/datafiles/setone/16781804094547_hinh-kim-cuong-goc.png'),
+          ),
+        ),
       ),
     ),
   );
